47f1d262e374f6f651de410ca4dbaaa040a02ee0
a7axD <44978645+a7axD@users.noreply.github.com>
2018-12-05 20:37:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,10 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "p";
 
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
   if (message.content.startsWith(adminprefix + 'setg')) {
     client.user.setGame(argresult);
       message.channel.send(`**✅   ${argresult}**`)
