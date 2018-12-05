2340da4b7cb17ddf3d7541107c8abaac562f57ef
a7axD <44978645+a7axD@users.noreply.github.com>
2018-12-05 20:35:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "p";
-var adminprefix = 'p'
-
 
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
